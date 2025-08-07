7fdc94a46b842ec76ff79b47d64f24aee2d2e20e
Anthony_Wendy <76909963+AnthonyWendy@users.noreply.github.com>
2025-08-07 19:44:22 -0300

    feat: created correct version of design pattern singleton

---
+++ desing_patterns/singleton/correct/Main.java
@@ -0,0 +1,21 @@
+package desing_patterns.singleton.correct.Singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n-- Design Pattern Singleton - WRONG VERSION --\n");
+
+        Singleton instanceOne = new Singleton();
+        Singleton instanceTwo = new Singleton();
+
+        Singleton instanceThree = Singleton.getInstance();
+        Singleton instanceFour = Singleton.getInstance();
+
+        System.out.println("\nVerificando se as instâncias são as mesmas:");
+        System.out.println("instanceOne == instanceTwo? " + (instanceOne == instanceTwo));
+        System.out.println("instanceThree == instanceFour? " + (instanceThree == instanceFour));
+
+        System.out.println("\nCreade Singleton instanc successfully.");
+    }
+}
---
+++ desing_patterns/singleton/correct/Singleton.java
@@ -0,0 +1,17 @@
+package desing_patterns.singleton.wrong.Singleton;
+
+public class Singleton {
+
+    private static Singleton uniqueInstance;
+
+    private Singleton() {
+        System.out.println("Instância criada: " + this);
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
