1f80e227977c0f35bec5325b3eb5d9fad361b417
Anthony_Wendy <76909963+AnthonyWendy@users.noreply.github.com>
2025-08-07 19:54:14 -0300

    fix: adjusted  logical error

--- desing_patterns/singleton/correct/Singleton.java
+++ desing_patterns/singleton/correct/Singleton.java
@@ -1,17 +1,19 @@
-package desing_patterns.singleton.wrong.Singleton;
+public class Main {
 
-public class Singleton {
+    public static void main(String[] args) {
 
-    private static Singleton uniqueInstance;
+        System.out.println("\n-- Design Pattern Singleton - CORRECT VERSION --\n");
 
-    private Singleton() {
-        System.out.println("Instância criada: " + this);
-    }
+        // Isso não compila se o construtor for privado
+        // Singleton instanceOne = new Singleton();
+        // Singleton instanceTwo = new Singleton();
 
-    public static synchronized Singleton getInstance() {
-        if (uniqueInstance == null) {
-            uniqueInstance = new Singleton();
-        }
-        return uniqueInstance;
+        Singleton instanceOne = Singleton.getInstance();
+        Singleton instanceTwo = Singleton.getInstance();
+
+        System.out.println("\nVerificando se as instâncias são as mesmas:");
+        System.out.println("instanceOne == instanceTwo? " + (instanceOne == instanceTwo));
+
+        System.out.println("\nCreated Singleton instance successfully.");
     }
 }
